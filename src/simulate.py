8d4d80bc82f8a414cee0e6adc3b1d200b56f8e29
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-03-25 19:34:25 +0530

    Adding RK4 integration

--- src/simulate.py
+++ src/simulate.py
@@ -5,23 +5,44 @@ from utils.helpers import *
 from utils.vtk_interface import VTKInterface
 
 # Assuming the object is glass
-constants = [1.04E8, 1.04E8, 0, 6760]
+constants = [1.04E4, 1.04E4, 0, 6760]
 density = 2588
-time_step = 0.1
+time_step = 0.01
 
-m_points, cells = VTKInterface.read('data/sphere.1.vtk')
-points = np.copy(m_points)
+m_points, cells = VTKInterface.read('data/cube.2.vtk')
+volume = get_volume(cells, m_points)
+mass = get_mass(cells, m_points, density)
+positions = np.copy(m_points)
+positions[:, 2] = 1.1 * positions[:, 2]
 beta = get_beta(cells, m_points)
 velocities = np.zeros(m_points.shape)
-gravity = -1 * np.array([0, 9.8, 0])
+gravity = -1 * np.array([0, 0, 0])
 
-for i in np.arange(0, 10, time_step):
-    dpos, dvel = get_derivatives(cells, points, velocities, beta)
-    stress = get_stress(dpos, dvel, constants)
-    internal_f = get_internal(cells, points, beta, stress)
-    mass = get_mass(cells, points, density)
-    internal_acc = internal_f / mass[:, None]
-    acceleration = np.add(internal_acc, gravity)
-    points = np.add(points, time_step * velocities)
-    velocities = np.add(velocities, time_step * acceleration)
-    VTKInterface.write(points, cells, 'output/sphere' + str(int(i * 10)) + '.vtk')
+
+def get_acc(points, velocities):
+    """The function makes things easier to type."""
+    global cells
+    global volume
+    global mass
+    global beta
+    global constants
+    return get_accel(cells, points, velocities, volume, mass, beta, constants)
+
+acceleration = get_acc(positions, velocities)
+h = time_step
+h2 = h / 2.0
+h6 = h / 6.0
+for i in range(1000):
+    q1 = velocities + h2 * acceleration
+    k1 = get_acc(positions + h2 * velocities, q1)
+    q2 = velocities + h2 * k1
+    k2 = get_acc(positions + h2 * q1, q2)
+    q3 = velocities + h * k2
+    k3 = get_acc(positions + h * q2, q3)
+    positions = positions + h * (velocities + h6 * (acceleration + k1 + k2))
+    velocities = velocities + h6 * (acceleration + 2 * k1 + 2 * k2 + k3)
+    acceleration = get_acc(positions, velocities)
+    # if stress.any() != 0:
+    #     import pdb
+    #     pdb.set_trace()
+    VTKInterface.write(positions, cells, 'output/sphere' + str(i) + '.vtk')
--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -4,6 +4,16 @@ import numpy as np
 from numpy.linalg import inv
 
 
+def get_accel(cells, points, velocities, volume, mass, beta, constants):
+    """The function returns the acceleration."""
+    dpos, dvel = get_derivatives(cells, points, velocities, beta)
+    stress, elastic, viscous, strain, rate = get_stress(dpos, dvel, constants)
+    internal_f = get_internal(cells, points, beta, stress, volume)
+    internal_acc = internal_f / mass[:, None]
+    return internal_acc
+    # acceleration = np.add(internal_acc, gravity)
+
+
 def point_vectors(cells, points):
     """Return vectors used in other functions."""
     p1 = points[cells[:, 0]]
@@ -30,10 +40,11 @@ def get_beta(cells, points):
     return inv(orig)
 
 
-def get_derivatives(cells, positions, velocities, beta):
+def get_derivatives(cells, points, velocities, beta):
     """Vectorised computation of derivatives, equation (21) and (22)."""
-    p1, p2, p3, p4 = point_vectors(cells, positions)
+    p1, p2, p3, p4 = point_vectors(cells, points)
     v1, v2, v3, v4 = point_vectors(cells, velocities)
+
     positions = np.stack([p1, p2, p3, p4], axis=2)
     velocities = np.stack([v1, v2, v3, v4], axis=2)
     delta = np.array([[1, 0, 0],
@@ -42,8 +53,8 @@ def get_derivatives(cells, positions, velocities, beta):
                       [0, 0, 0]])
     prod1 = np.matmul(beta, delta)
     # einsum() applies a matrix multiplication across dimension 2 and 3
-    dpos = np.einsum('ijk,ikl->ijl', positions, prod1)
-    dvel = np.einsum('ijk,ikl->ijl', velocities, prod1)
+    dpos = np.matmul(positions, prod1)
+    dvel = np.matmul(velocities, prod1)
     return dpos, dvel
 
 
@@ -53,12 +64,12 @@ def get_stress(dpos, dvel, constants):
 
     constants - (lambda, mu, phi, psi) - equation (7), (8)
     """
-    dpos_T = np.transpose(dpos, [0, 2, 1])
-    dvel_T = np.transpose(dvel, [0, 2, 1])
-    strain = np.einsum('ijk,ikl->ijl', dpos_T, dpos) - np.eye(3)
+    # dpos_T = np.transpose(dpos, [0, 2, 1])
+    # dvel_T = np.transpose(dvel, [0, 2, 1])
+    strain = np.einsum('ijk,ijl->ikl', dpos, dpos) - np.eye(3)
     rate = np.add(
-        np.einsum('ijk,ikl->ijl', dpos_T, dvel),
-        np.einsum('ijk,ikl->ijl', dvel_T, dpos)
+        np.einsum('ijk,ijl->ikl', dpos, dvel),
+        np.einsum('ijk,ijl->ikl', dvel, dpos)
     )
     # Implementation of equation (7)
     str_trace = np.trace(strain, axis1=1, axis2=2)
@@ -69,14 +80,13 @@ def get_stress(dpos, dvel, constants):
     viscous = \
         constants[2] * np.einsum('i,jk->ijk', rate_trace, np.eye(3)) + \
         2 * constants[3] * rate
-    return np.add(elastic, viscous)
+    return np.add(elastic, viscous), elastic, viscous, strain, rate
 
 
-def get_internal(cells, points, beta, stress):
+def get_internal(cells, points, beta, stress, volume):
     """Compute internal forces on all nodes."""
     p1, p2, p3, p4 = point_vectors(cells, points)
     positions = np.stack([p1, p2, p3, p4], axis=2)
-    volume = get_volume(cells, points)
     # This is because the fourth column of beta is not used in summation
     beta = beta[:, :, :3]
     # einsum() implementation of equation (27)
